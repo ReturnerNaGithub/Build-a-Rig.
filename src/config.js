95ab7d39d2676bd5cf8094f04093a0a6eab221ac
ReturnerNaGithub <78732470+ReturnerNaGithub@users.noreply.github.com>
2022-08-28 16:17:49 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
     "collection_address": "0x983961F34fC4cFC5EAfec371CDBA9d56fF8C1935",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
     "title": "Build a Rig",
